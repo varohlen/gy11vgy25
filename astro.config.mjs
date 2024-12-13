32af7088325437050cffeba00bf6deb912db1882
Viktor Arohlén <varohlen@gmail.com>
2024-12-13 13:03:03 +0100

    remove unused React integration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind()],
   output: 'server',  // Enable server-side rendering
   server: {
     port: 4322
